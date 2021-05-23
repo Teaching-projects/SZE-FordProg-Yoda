dc35a5b1e19ff196f0b346ba715347d1260e665b
AdamHorva <adam.horvath@mmsone.com>
2021-05-23 19:07:15 +0200

    Initial commit

---
+++ include/yoda/ll1_parser.hpp
@@ -0,0 +1,8 @@
+#include <string>
+
+class ll1_parser_s {
+	std::string input;
+	std::string scheme;
+
+	unsigned int pos;
+};
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "yoda/ll1_parser.hpp"
+
+int main(int argc, char** argv) {
+	return 0;
+}
